281219172eab4aad3bc68391ce2022f2d0a4a533
erdenerunl <erdenerunale@gmail.com>
2021-01-29 23:04:32 +0300

    Profile sub-tabs were created and styled.

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -20,7 +20,7 @@
       </button>
     </header>
     <div class="profile-body">
-      <perferct-scrollbar>
+      <perfect-scrollbar>
         <div class="px-4">
           <div class="text-center">
             <figure class="user-avatar">
@@ -36,54 +36,103 @@
               <li class="nav-item" role="presentation">
                 <a
                   class="nav-link active"
-                  id="home-tab"
+                  id="about-tab"
                   data-bs-toggle="tab"
-                  href="#home"
+                  href="#about"
                   role="tab"
-                  aria-controls="home"
+                  aria-controls="about"
                   aria-selected="true"
-                  >Home</a
+                  >About</a
                 >
               </li>
               <li class="nav-item" role="presentation">
                 <a
                   class="nav-link"
-                  id="profile-tab"
+                  id="media-tab"
                   data-bs-toggle="tab"
-                  href="#profile"
+                  href="#media"
                   role="tab"
-                  aria-controls="profile"
+                  aria-controls="media"
                   aria-selected="false"
-                  >Profile</a
+                  >Media</a
                 >
               </li>
             </ul>
             <div class="tab-content" id="myTabContent">
               <div
                 class="tab-pane fade show active"
-                id="home"
+                id="about"
                 role="tabpanel"
-                aria-labelledby="home-tab"
+                aria-labelledby="about-tab"
               >
-                <p class="m-3">
+                <p class="my-4">
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis
                   ducimus accusamus temporibus suscipit laudantium accusantium
                   quidem eaque autem voluptatem quod eligendi distinctio magnam
                   molestiae officiis at, vero cupiditate ad animi.
                 </p>
+                <div class="my-3">
+                  <h5 class="mt-3">Phone</h5>
+                  <p class="mb-4">0555 555 55 55</p>
+                </div>
+                <div class="my-3">
+                  <h5 class="mt-3">Settings</h5>
+                  <div class="form-group">
+                    <div class="form-item">
+                      <label class="switch">
+                        <input type="checkbox" id="blockUser" />
+                        <span class="slider round"></span>
+                      </label>
+                      <span>Block</span>
+                    </div>
+                    <div class="form-item">
+                      <label class="switch">
+                        <input type="checkbox" id="blockUser" />
+                        <span class="slider round"></span>
+                      </label>
+                      <span>Mute</span>
+                    </div>
+                    <div class="form-item">
+                      <label class="switch">
+                        <input type="checkbox" id="blockUser" />
+                        <span class="slider round"></span>
+                      </label>
+                      <span>Get Notification</span>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div
                 class="tab-pane fade"
-                id="profile"
+                id="media"
                 role="tabpanel"
-                aria-labelledby="profile-tab"
+                aria-labelledby="media-tab"
               >
-                ...
+               <h5 class="recent-files">
+                   <span>Recent Files</span>
+                   <a href="#">Upload</a>
+               </h5>
+               <div class="div">
+                   <ul class="list-group list-group-flush">
+                       <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                           <a href="#">ExampleFile</a>
+                       </li>
+                       <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                           <a href="#">ExampleFile</a>
+                       </li>
+                       <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                           <a href="#">ExampleFile</a>
+                       </li>
+                       <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                           <a href="#">ExampleFile</a>
+                       </li>
+                   </ul>
+               </div>
               </div>
             </div>
           </div>
         </div>
-      </perferct-scrollbar>
+      </perfect-scrollbar>
     </div>
   </div>
 </template>
@@ -124,5 +173,89 @@ small {
   color: #00d4ff;
   border: none;
   border-bottom: 2px solid #00d4ff;
+}
+.form-group {
+  margin-bottom: 1rem;
+
+  .form-item {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.8rem;
+
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 48px;
+      height: 21px;
+    }
+
+    .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 16px;
+      width: 16px;
+      left: 3px;
+      bottom: 3px;
+      background-color: white;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+
+    input:checked + .slider {
+      background-color: #2196f3;
+    }
+
+    input:focus + .slider {
+      box-shadow: 0 0 1px #2196f3;
+    }
+
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 16px;
+    }
+
+    .slider.round:before {
+      border-radius: 50%;
+    }
+  }
+}
+.ps{
+    position: relative;
+    height: calc(100vh - 170px);
+    overflow: hidden;
+}
+.recent-files{
+    display: flex;
+    justify-content: space-between;
+    margin: 3rem 0;
+
+    a{  
+        color: #007bff;
+        font-size: 14px;
+    }
 }
 </style>
